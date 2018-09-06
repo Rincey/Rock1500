1c448b6f6c293bbff0cbf2b8469650b0374e3864
Rincey <rince69@gmail.com>
2018-09-06 21:18:09 +1200

    Biggest jump - eliminate already played

--- Find-GreatestRise.ps1
+++ Find-GreatestRise.ps1
@@ -1,32 +1,23 @@
-$jump = 1303
-$lastYear = Import-Csv ".\Full 2017.csv"
-$candidatesTemp = $lastYear | Where-Object {[int]($_.rank) -ge $jump}
-
-$candidates = @()
-foreach ($item in $candidatesTemp) {
-    $row = "" | Select-Object title, artist
-    #$row.rank = $item.rank
-    $row.title = $item.title
-    $row.artist = $item.artist
-    $candidates += $row
-}
-
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$AlreadyPlayedTemp = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
-$AlreadyPlayed = @()
-foreach ($item in $AlreadyPlayedTemp) {
-    $row = "" | Select-Object title, artist
-    #$row.rank = $item.rank
-    $row.title = $item.title
-    $row.artist = $item.artist
-    $AlreadyPlayed += $row
-}
 
+$jump = 1303
 $possibles = @()
+$AlreadyPlayed = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
+$lastYear = Import-Csv ".\Full 2017.csv"
+$candidates = $lastYear | Where-Object {[int]($_.rank) -gt $jump}
+
+# Remove already played
 foreach ($track in $candidates) {
-    if ($AlreadyPlayed -contains $track) {
+    $flag = $false
+    foreach ($apTrack in $AlreadyPlayed) {
+        if (($apTrack.artist -eq $track.artist) -and ($apTrack.title -eq $track.title)) {
+            $flag = $true
+        }
+    }
+    if (!($flag)) {
         $possibles += $track
     }
 }
 
-$possibles |Format-Table -AutoSize artist,title
+
+$possibles |Format-Table -AutoSize rank,artist, title
