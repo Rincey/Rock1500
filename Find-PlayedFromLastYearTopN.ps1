8e1f654311c73bc6e5c0ffe04d8321a7ab24bd2b
Rincey <rince69@gmail.com>
2018-09-13 14:30:26 +1200

    add find top N

---
+++ Find-PlayedFromLastYearTopN.ps1
@@ -0,0 +1,19 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$N = 500
+$AlreadyPlayed = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
+$lastYear = Import-Csv ".\Full 2017.csv"
+
+$candidates = $lastyear[0..($N - 1)] 
+$count = 0
+
+foreach ($track in $candidates) {
+    foreach ($apTrack in $AlreadyPlayed) {
+        if (($apTrack.artist -eq $track.artist) -and ($apTrack.title -eq $track.title)) {
+            "($($Track.rank)) $($apTrack.rank): $($apTrack.artist) - $($apTrack.title)"
+            $count += 1
+        }
+    }
+
+}
+"Total: $count"
