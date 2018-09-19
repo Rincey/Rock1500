f6a381bf1e54de09cd03e82e0907978ddeddf8f4
Rincey <rince69@gmail.com>
2018-09-19 13:26:18 +1200

    refactor Top N

--- Find-PlayedFromLastYearTopN.ps1
+++ Find-PlayedFromLastYearTopN.ps1
@@ -1,22 +1,19 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-
+$ErrorActionPreference ="SilentlyContinue"
 $N = 100
 $AlreadyPlayed = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
-$lastYear = Import-Csv ".\Full 2017.csv"
 
-$candidates = $lastyear[0..($N - 1)] 
+
 $count = 0
 "Top $N from last year already played
 
 (last year) this year: Artist - Title"
-
-foreach ($track in $candidates) {
-    foreach ($apTrack in $AlreadyPlayed) {
-        if (($apTrack.artist -eq $track.artist) -and ($apTrack.title -eq $track.title)) {
-            "($($Track.rank)) $($apTrack.rank): $($apTrack.artist) - $($apTrack.title)"
-            $count += 1
-        }
+foreach ($track in $AlreadyPlayed){
+    if ([int]$track.rankOneYearAgo -le $N){
+        "($($Track.rankOneYearAgo)) $($Track.rank): $($Track.artist) - $($Track.title)"
+        $count += 1  
     }
-
 }
 "Total: $count"
+
+$ErrorActionPreference = "Continue"
