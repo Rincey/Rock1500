3c3e06840ffe37b62df3b1a16dc2debbb64d810f
Rincey <rince69@gmail.com>
2018-09-19 21:41:08 +1200

    greatest rise include note if winner

--- Find-GreatestRise.ps1
+++ Find-GreatestRise.ps1
@@ -9,16 +9,23 @@ $candidates = $lastYear | Where-Object {[int]($_.rank) -gt $jump}
 # Remove already played
 foreach ($track in $candidates) {
     $flag = $false
+    $winnerFlag = $false
     foreach ($apTrack in $AlreadyPlayed) {
         if (($apTrack.artist -eq $track.artist) -and ($apTrack.title -eq $track.title)) {
             $flag = $true
+        }
+        if (([int]$Track.rank - [int]$aptrack.rank) -eq $jump) {
+            $winnerFlag = $true
+            $winner = $track
         }
     }
     if (!($flag) -and ([int]$AlreadyPlayed[0].rank -gt [int]$track.rank-$jump)) {
         $possibles += $track
     }
 }
-
+if ($winnerFlag) {
+    "Winner: $winner"
+}
 $soonest = [int]$possibles[-1].rank - $jump
 
 $possibles |Format-Table -AutoSize rank,artist, title
