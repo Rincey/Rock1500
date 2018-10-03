3679adc462dc20ff44d44abae0445d932deba879
Rincey <rince69@gmail.com>
2018-10-03 19:16:30 +1300

    create playlist script

--- Create-SpotifyPLaylist.ps1
+++ Create-SpotifyPLaylist.ps1
@@ -4,11 +4,15 @@
 #$Rock1500_2018 = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
 $Rock1500_2018 = (Get-Content '.\Full 2018.csv' | ConvertFrom-Csv)
 
-Connect-Spotify -ClientIdSecret (Import-Clixml ..\spotify.credential) -KeepCredential -Debug -RedirectUri http://localhost:8001
+Connect-Spotify -ClientIdSecret (Import-Clixml ..\spotify.credential2) -KeepCredential -Debug -RedirectUri http://localhost:8001
 
-$playlist = New-SpotifyPlaylist -Name "Rock 1500 (2018)"
-"Rank,CountdownTitle,CountdownArtist,SpotifyName,SpotifyArtist"
-foreach ($track in $Rock1500_2018[0..19]) {
+$playlist = Get-SpotifyPlaylist -Name "Rock 1500 (2018)" -My
+
+foreach ($track in $Rock1500_2018) {
     $spotifyTrack = Find-SpotifyTrack -filter "$($track.title) $($track.artist) $($track.album)" -Limit 1
-    "$($track.rank),$($track.title),$($track.artist),$($spotifyTrack.name),$($spotifyTrack.artists)"
+    "$($track.rank) $($track.title) $($track.artist) $($spotifyTrack.count)"
+    if ($spotifyTrack.count -eq 1) {
+         Add-SpotifyTracktoPlaylist -Id $playlist.Id -Tracks $spotifyTrack.uri
+    }
+    
 }
