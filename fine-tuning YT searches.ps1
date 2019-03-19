3ed4079730538e35be93b443f103bf1ea6a99ac2
Rincey <rince69@gmail.com>
2019-03-19 14:12:03 +1300

    finetune YT searches

---
+++ fine-tuning YT searches.ps1
@@ -0,0 +1,36 @@
+﻿##### Get through proxy if necessary
+(New-Object System.Net.WebClient).Proxy.Credentials = [System.Net.CredentialCache]::DefaultNetworkCredentials
+
+$youtube = 'https://www.youtube.com' 
+$line = "" | select title,artist
+$line.title = "Hardwired"
+$line.artist = "Metallica"
+
+ $searchTerm = ($line.title -replace " ","+") + "+" + ($line.artist -replace " ","+")
+ $youTubePage = "$youtube" + "/results?search_query=" + $searchTerm
+ $web = Invoke-WebRequest -Uri "$youTubePage"
+ $allLinks = New-Object System.Collections.ArrayList
+ foreach($link in $web.Links)
+      {
+       if($searchTerm -like "*cover*") {
+ $a = ($web.Links |?{($_.href -like '*/watch?v=*') -and 
+      ($_.href -notlike '*list=*') -and
+      (($_.innerText -like "*official*") -or ($_.innerText -notlike "*album*")) -and
+      ($_.innerText -notlike "*:*") -and
+      ($_.innerText -notlike "*lyrics*")})
+      }
+    else
+    {
+ $a = ($web.Links |?{($_.href -like '*/watch?v=*') -and 
+      ($_.href -notlike '*list=*') -and
+      (($_.innerText -like "*official*") -or ($_.innerText -notlike "*album*")) -and
+      ($_.innerText -notlike "*:*") -and
+      ($_.innerText -notlike "*lyrics*") -and
+      ($_.innerText -notlike "*cover*")})
+    }         
+       
+         $url = $youtube + $a.href  
+         $linkEntry = "$($line.rank) ; $($line.artist) ; $($line.title) ; $url ; $($a.innerText) "
+         [void]$allLinks.Add($linkEntry)
+}      
+      
