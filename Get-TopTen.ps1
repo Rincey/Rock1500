743d629cd923a07fccc64c0fdf57761f6995b886
Rincey <rince69@gmail.com>
2019-03-25 11:29:57 +1300

    updates

--- Get-TopTen.ps1
+++ Get-TopTen.ps1
@@ -1,8 +1,11 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$topTen = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | 
-    convertfrom-json) |
+
+$countdown = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | 
+    convertfrom-json)
+
+    $topTen = $countdown |
     Group-Object artist |
     sort-object count -Descending
 
 $topten| Where-Object {$_.count -ge $topten[9].count} | Format-Table Name,Count
-
+$countdown[0..10] | Format-Table -a rank,artist,title
