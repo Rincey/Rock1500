1e8967e073b3fc51a720891f48c74cf9b906d093
Rincey <rince69@gmail.com>
2019-08-28 19:56:23 +1200

    adding per day check in topten

--- Get-TopTen.ps1
+++ Get-TopTen.ps1
@@ -1,17 +1,22 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+param (
+    [switch]$day
+)
 
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $rawcountdown = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | 
     convertfrom-json)
+
+
 if ($day) {
-    $countdown = $rawcountdown | Where-Object { $_.timestamp -like "*$day*" }
-}
-else {
-    $countdown = $rawcountdown
-}
+    
+    $newcountdown = $rawcountdown | select *,@{n='date';e={$_.timestamp.split(" ")[0]}}
+    $newcountdown | Group-Object date
+    
 
-$topTen = $countdown |
-Group-Object artist |
-sort-object count -Descending
+    $topTen = $countdown |
+    Group-Object artist |
+    sort-object count -Descending
 
-$topten | Where-Object { $_.count -ge $topten[9].count } | Select-Object Name, Count | Out-GridView -Title "Top Ten"
+    $topten | Where-Object { $_.count -ge $topten[9].count } | Select-Object Name, Count | Out-GridView -Title "Top Ten"
+}
 
