6caae2e4dc6971459d143b0f7ca33d9508e846e0
Rincey <rince69@gmail.com>
2019-09-06 08:53:09 +1200

    played tweaks

--- Get-PlayedTracksJSON.ps1
+++ Get-PlayedTracksJSON.ps1
@@ -1,3 +1,23 @@
+Function Get-TotalWeekDays {
+    [cmdletbinding()]
+    Param (
+        [Parameter(Position = 0, Mandatory = $True, HelpMessage = "What is the start date?")]
+        [ValidateNotNullorEmpty()]
+        [DateTime]$Start,
+        [Parameter(Position = 1, Mandatory = $True, HelpMessage = "What is the end date?")]
+        [ValidateNotNullorEmpty()]
+        [DateTime]$End
+    )
+  
+    $i = 0
+    for ($d = $Start; $d -le $end; $d = $d.AddDays(1)) {
+        if ($d.DayOfWeek -notmatch "Sunday|Saturday") {
+            $i++    
+        }
+    } 
+    return    $i
+} 
+
 $MyVotes = get-content .\picks2019.json | ConvertFrom-Json
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $AlreadyPlayed = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
@@ -13,9 +33,9 @@ Vote Set '$($VoteSet)':"
 
         if ($MyVotes.picks.$VoteSet -match $song) {
         
-
+$countdownday = Get-TotalWeekDays -Start "2019-08-25" -End $($track.timestamp.split(" ")[0])
             "`"$($song.artist) - $($song.title)`"
-Played at $($track.timestamp.split(" ")[1]) on $($track.timestamp.split(" ")[0])
+Played at $($track.timestamp.split(" ")[1]) on $($track.timestamp.split(" ")[0]) (Day $countdownday)
 Number $($track.rank)
         "
             $count += 1
