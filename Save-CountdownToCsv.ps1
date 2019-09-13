01f02e2a689a016d67103a851389712806281150
Rincey <rince69@gmail.com>
2019-09-13 22:36:05 +1200

    save 2019 countdown

--- Save-CountdownToCsv.ps1
+++ Save-CountdownToCsv.ps1
@@ -1,3 +1,4 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+$year = get-date -Format yyyy
 $Rock1500 = ((Invoke-WebRequest https://radio-api.mediaworks.nz/comp-api/v1/countdown/therock -UseBasicParsing).content | convertfrom-json)
-$Rock1500.GetEnumerator() | Export-Csv '.\Full 2018.csv' -NoTypeInformation
+$Rock1500.GetEnumerator() | Export-Csv ".\Full $year.csv" -NoTypeInformation
