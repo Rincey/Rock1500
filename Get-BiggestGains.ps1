5e5273b249e2efd952d0e4335a8aec4461089209
Rincey <rince69@gmail.com>
2021-12-09 11:41:20 +1300

    2021

--- Get-BiggestGains.ps1
+++ Get-BiggestGains.ps1
@@ -11,6 +11,6 @@ ConvertFrom-Json
 
 $countdown = $rawcountdown | Select-Object *,@{n='Change';e={$_.rankOneYearAgo - $_.rank}}
 
-$countdown | Sort-Object change -Descending | Select-Object -First 10 rank,title,artist,change | Out-GridView -Title "Biggest Gains"
-$countdown | Sort-Object change  | Select-Object -First 10 rank,title,artist,change | Out-GridView -Title "Biggest Losers"
+$countdown | Sort-Object change -Descending | Select-Object -First 20 rank,title,artist,change | Out-GridView -Title "Biggest Gains"
+$countdown | Sort-Object change  | Select-Object -First 20 rank,title,artist,change | Out-GridView -Title "Biggest Losers"
 $countdown | Where-Object{$_.change -eq 0} | Select-Object rank,title,artist | Out-GridView -Title "No Change"
