f422df464c9048a8f679648b070e06cc8f920f00
Rincey <rince69@gmail.com>
2021-12-23 09:40:44 +1300

    untappd

---
+++ get-UntappdRSS.ps1
@@ -0,0 +1,45 @@
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+if ($PSScriptRoot) {
+    $reportfolder = $PSScriptRoot
+}
+else {
+    $reportfolder = $psEditor.GetEditorContext().CurrentFile.Path.replace($psEditor.GetEditorContext().CurrentFile.Path.split("\")[-1], "")
+}
+$filename = "$reportfolder\..\untappdkey.txt"
+$key = Get-Content $filename
+
+$url = "https://untappd.com/rss/user/Rincey?key=$key"
+
+$result = Invoke-RestMethod `
+    -Uri $url `
+    -Method get 
+
+$regex = @"
+<div class="caps" data-rating=".*">
+"@
+
+foreach ($link in $result) {
+
+    $checkinurl = $link.link
+    $checkinresult = Invoke-RestMethod `
+        -Uri $checkinurl `
+        -Method get 
+
+
+    if ($checkinresult -match $regex) {
+        $rating = $matches[0].split("=")[2].split("`"")[1]
+    }
+    else {
+        $rating = "No Rating"
+    }
+
+    $beer = $link.title -replace "Scott w. is drinking ", ""
+    $beer = $beer -replace " at Untappd at Home", ""
+
+    Write-Output "$beer,$rating"
+
+} 
+
+
